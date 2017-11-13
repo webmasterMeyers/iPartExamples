bee6962b767b4860d81044a2563841a4c4594ba6
Chad Meyers <chad@nobodyfamous.ca>
2017-11-13 10:21:53 -0400

    Add Hide Config Options

--- webform-based/asiComplexTemplate/$safeprojectname$ConfigEdit.ascx.cs
+++ webform-based/asiComplexTemplate/$safeprojectname$ConfigEdit.ascx.cs
@@ -1,4 +1,6 @@
 ﻿using Asi.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
 
 namespace $safeprojectname$
 {
@@ -12,10 +14,33 @@ namespace $safeprojectname$
         {
             get
             {
-                // TODO: This must match the 'Name of the Content Type' defined on the Content Type in
-                // TODO: 'Content Manager->Maintenance->Content types'
+                // TODO:
+                // This must match the 'Name of the Content Type' defined on the Content Type in
+                // 'Content Manager->Maintenance->Content types'
                 return "$safeprojectname$";
             }
+        }
+        protected override void OnInit(EventArgs e)
+        {
+            HideConfigurationOptions();
+            base.OnInit(e);
+        }
+        private void HideConfigurationOptions()
+        {
+            if (HiddenConfigurationOptions == null)
+                HiddenConfigurationOptions = new List<HideConfiguration>();
+            // To Hide Default Options, uncomment lines below    
+            // HiddenConfigurationOptions.Add(HideConfiguration.PartTitle);
+            // HiddenConfigurationOptions.Add(HideConfiguration.CssClass);
+            // HiddenConfigurationOptions.Add(HideConfiguration.ShowBorder);
+            // HiddenConfigurationOptions.Add(HideConfiguration.Collapsible);
+            // HiddenConfigurationOptions.Add(HideConfiguration.DisplayOnExtraSmallScreens);
+            // HiddenConfigurationOptions.Add(HideConfiguration.DisplayOnSmallScreens);
+            // HiddenConfigurationOptions.Add(HideConfiguration.DisplayOnMediumScreens);
+            // HiddenConfigurationOptions.Add(HideConfiguration.DisplayOnLargeScreens);
+            // HiddenConfigurationOptions.Add(HideConfiguration.DoNotRenderInDesignMode);
+            // HiddenConfigurationOptions.Add(HideConfiguration.ModuleSpecificSetting);
+            EnsureChildControls();
         }
     }
 }
